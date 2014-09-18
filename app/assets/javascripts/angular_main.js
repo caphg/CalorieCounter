1511f3b1f31dae5b700321e0cd94b1ff67f4cf17
Hrvoje Grdic <hrvoje.grdic@ericsson.com>
2014-09-18 15:56:12 +0200

    editing entries

--- app/assets/javascripts/angular_main.js
+++ app/assets/javascripts/angular_main.js
@@ -19,7 +19,12 @@ app.filter('firstLetter', function() {
 });
 
 app.controller('CaloriesController', ['$scope', '$http',  function($scope, $http) {
+    $scope.editingMeal = false;
+    $scope.editingCalories = false;
+    $scope.editingDate = false;
+    $scope.editingDescription = false;
 
+    $scope.entry = {};
 
     $scope.init = function () {
         //fetch data
@@ -31,5 +36,72 @@ app.controller('CaloriesController', ['$scope', '$http',  function($scope, $http
         });
     };
 
+    $scope.doneEditingMeal = function(id){
+        $http({
+            url: "/entries/"+id+".json",
+            data: $scope.entry,
+            method: "PATCH"
+        }).success(function () {
+            $scope.editingMeal = false;
+        }).fail(function (){
+            log("error updating meal");
+        });
+    };
+
+    $scope.startEditingMeal = function(val){
+        $scope.editingMeal = true;
+        $scope.entry = val;
+    };
+
+    $scope.doneEditingCalories = function(id){
+        $http({
+            url: "/entries/"+id+".json",
+            data: $scope.entry,
+            method: "PATCH"
+        }).success(function () {
+            $scope.editingCalories = false;
+        }).fail(function (){
+            log("error updating calories");
+        });
+    };
+
+    $scope.startEditingCalories = function(val){
+        $scope.editingCalories = true;
+        $scope.entry = val;
+    };
+
+    $scope.doneEditingDate = function(id){
+        $http({
+            url: "/entries/"+id+".json",
+            data: $scope.entry,
+            method: "PATCH"
+        }).success(function () {
+            $scope.editingDate = false;
+        }).fail(function (){
+            log("error updating date");
+        });
+    };
+
+    $scope.startEditingDate = function(val){
+        $scope.editingDate = true;
+        $scope.entry = val;
+    };
+
+    $scope.doneEditingDescription = function(id){
+        $http({
+            url: "/entries/"+id+".json",
+            data: $scope.entry,
+            method: "PATCH"
+        }).success(function () {
+            $scope.editingDescription = false;
+        }).fail(function (){
+            log("error updating description");
+        });
+    };
+
+    $scope.startEditingDescription = function(val){
+        $scope.editingDescription = true;
+        $scope.entry = val;
+    };
 
 }]);
