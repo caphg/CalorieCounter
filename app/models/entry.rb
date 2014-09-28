29f8ef33afa19e0002b73fe1524b9729cd2d4de1
vhailor <caphg@hotmail.com>
2014-09-28 14:04:46 +0200

    readme update, entries order modified

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
 
+  validates :user, presence: true
   validates :meal, presence: true, length: {minimum: 2, maximum: 50}
   validates :calories, presence: true, numericality: {greater_than_or_equal_to: 0, less_than: 100000}
   validates :date, presence: true
